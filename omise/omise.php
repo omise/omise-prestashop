93ae704ffb5ed3cf58ab33001054540d7530ea2b
Saroj Saengphongumphai <ss.saroj@gmail.com>
2016-09-06 12:13:24 +0700

    Display the initial setting page
    It is only display the controls. It has not been any function.

--- omise/omise.php
+++ omise/omise.php
@@ -41,4 +41,100 @@ class Omise extends PaymentModule
         $this->displayName = self::MODULE_DISPLAY_NAME;
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall ' . self::MODULE_DISPLAY_NAME . ' module?');
     }
+
+    public function getContent()
+    {
+        $fields_form[0]['form'] = array(
+            'legend' => array(
+                'title' => $this->l('Settings')
+            ),
+            'input' => array(
+                array(
+                    'type' => 'switch',
+                    'label' => $this->l('Enable/Disable'),
+                    'name' => 'module',
+                    'is_bool' => true,
+                    'desc' => $this->l('Enable Omise Payment Module.'),
+                    'values' => array(
+                        array(
+                            'id' => 'module_enabled',
+                            'value' => 1,
+                            'label' => 'Enabled'
+                        ),
+                        array(
+                            'id' => 'module_disabled',
+                            'value' => 0,
+                            'label' => 'Disabled'
+                        )
+                    )
+                ),
+                array(
+                    'type' => 'switch',
+                    'label' => $this->l('Sandbox'),
+                    'name' => 'sandbox',
+                    'is_bool' => true,
+                    'desc' => $this->l('Enabling sandbox means that all your transactions will be in TEST mode.'),
+                    'values' => array(
+                        array(
+                            'id' => 'sandbox_on',
+                            'value' => 1,
+                            'label' => 'Enabled'
+                        ),
+                        array(
+                            'id' => 'sandbox_off',
+                            'value' => 0,
+                            'label' => 'Disabled'
+                        )
+                    )
+                ),
+                array(
+                    'type' => 'text',
+                    'label' => $this->l('Public key for test'),
+                    'name' => 'publicKeyForTest',
+                    'required' => false,
+                    'desc' => 'The "Test" mode public key can be found in Omise Dashboard.'
+                ),
+                array(
+                    'type' => 'text',
+                    'label' => $this->l('Secret key for test'),
+                    'name' => 'secretKeyForTest',
+                    'required' => false,
+                    'desc' => 'The "Test" mode secret key can be found in Omise Dashboard.'
+                ),
+                array(
+                    'type' => 'text',
+                    'label' => $this->l('Public key for live'),
+                    'name' => 'publicKeyForLive',
+                    'required' => false,
+                    'desc' => 'The "Live" mode public key can be found in Omise Dashboard.'
+                ),
+                array(
+                    'type' => 'text',
+                    'label' => $this->l('Secret key for live'),
+                    'name' => 'secretKeyForLive',
+                    'required' => false,
+                    'desc' => 'The "Live" mode secret key can be found in Omise Dashboard.'
+                ),
+                array(
+                    'label' => '<b>Advance Settings</b>'
+                ),
+                array(
+                    'type' => 'text',
+                    'label' => $this->l('Title'),
+                    'name' => 'title',
+                    'required' => false,
+                    'desc' => 'This controls the title which the user sees during checkout.'
+                )
+            ),
+            'submit' => array(
+                'title' => $this->l('Save'),
+                'class' => 'btn btn-default pull-right'
+            )
+        );
+
+        $helper = new HelperForm();
+        $helper->submit_action = 'submit' . $this->name;
+
+        return $helper->generateForm($fields_form);
+    }
 }
