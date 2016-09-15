ae13ede15c34c6f78447b090f4163126821fb84a
guzzilar <iam.nuttanon@gmail.com>
2016-09-15 19:33:35 +0700

    :lipstick: code styling

--- omise/omise.php
+++ omise/omise.php
@@ -27,17 +27,17 @@ class Omise extends PaymentModule
 
     public function __construct()
     {
-        $this->name = self::MODULE_NAME;
-        $this->tab = 'payments_gateways';
-        $this->version = self::MODULE_VERSION;
-        $this->author = 'Omise';
-        $this->need_instance = 0;
+        $this->name                   = self::MODULE_NAME;
+        $this->tab                    = 'payments_gateways';
+        $this->version                = self::MODULE_VERSION;
+        $this->author                 = 'Omise';
+        $this->need_instance          = 0;
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.6');
-        $this->bootstrap = true;
+        $this->bootstrap              = true;
 
         parent::__construct();
 
-        $this->displayName = self::MODULE_DISPLAY_NAME;
-        $this->confirmUninstall = $this->l('Are you sure you want to uninstall ' . self::MODULE_DISPLAY_NAME . ' module?');
+        $this->displayName            = self::MODULE_DISPLAY_NAME;
+        $this->confirmUninstall       = $this->l('Are you sure you want to uninstall ' . self::MODULE_DISPLAY_NAME . ' module?');
     }
 }
