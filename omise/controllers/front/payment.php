9650492540863022f5701c225e23eb089fb054a3
Saroj Saengphongumphai <ss.saroj@gmail.com>
2017-01-07 17:37:39 +0700

    Remove dependencies that used to create charge
    Remove all dependecies that used to create Omise charge out from
    controller class.
    
    The function that used to create Omise charge has been implemented at
    the delegate class, Charge.

--- omise/controllers/front/payment.php
+++ omise/controllers/front/payment.php
@@ -3,10 +3,6 @@ if (! defined('_PS_VERSION_')) {
     exit();
 }
 
-require_once _PS_MODULE_DIR_ . '/omise/libraries/omise-php/lib/Omise.php';
-require_once _PS_MODULE_DIR_ . '/omise/libraries/omise-plugin/Omise.php';
-require_once _PS_MODULE_DIR_ . '/omise/setting.php';
-
 class OmisePaymentModuleFrontController extends ModuleFrontController
 {
     public $context;
