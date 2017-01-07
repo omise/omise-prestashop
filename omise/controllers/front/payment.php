244bd75593a17b7fc72add4c5a61fe34b28c5a4f
Saroj Saengphongumphai <ss.saroj@gmail.com>
2017-01-07 17:30:53 +0700

    Remove function that used to create charge request
    Remove functions that used to create Omise charge request out from
    controller class.
    
    Those functions have been implemented at the delegate class, Charge.

--- omise/controllers/front/payment.php
+++ omise/controllers/front/payment.php
@@ -34,42 +34,4 @@ class OmisePaymentModuleFrontController extends ModuleFrontController
 
         $this->setTemplate('payment_result.tpl');
     }
-
-    public function getAmount()
-    {
-        $currency_code = $this->getCurrencyCode();
-        $order_total = (float) $this->context->cart->getOrderTotal(true, Cart::BOTH);
-
-        return OmisePluginHelperCharge::amount($currency_code, $order_total);
-    }
-
-    public function getCardToken()
-    {
-        return Tools::getValue('omise_card_token');
-    }
-
-    public function getCapture()
-    {
-        return 'true';
-    }
-
-    public function getChargeDescription()
-    {
-        return 'Charge a card using a token from PrestaShop (' . _PS_VERSION_ . ')';
-    }
-
-    public function getCurrencyCode()
-    {
-        $currency_id = (int) $this->context->cart->id_currency;
-        $currency_instance = Currency::getCurrencyInstance($currency_id);
-
-        return $currency_instance->iso_code;
-    }
-
-    public function getSecretKey()
-    {
-        $setting = new Setting();
-
-        return $setting->getSecretKey();
-    }
 }
