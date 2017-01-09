b691d9a7bea49feb3eac2affc6de38404067bd53
Saroj Saengphongumphai <ss.saroj@gmail.com>
2017-01-09 14:00:18 +0700

    Implement a function to save the order

--- omise/classes/payment_order.php
+++ omise/classes/payment_order.php
@@ -5,4 +5,93 @@ if (! defined('_PS_VERSION_')) {
 
 class PaymentOrder extends ModuleFrontController
 {
+    protected function getCartId()
+    {
+        $cart = $this->context->cart;
+        return (int) $cart->id;
+    }
+
+    protected function getCartOrderTotal()
+    {
+        $cart = $this->context->cart;
+        return (float) $cart->getOrderTotal();
+    }
+
+    protected function getCustomerSecureKey()
+    {
+        $cart = $this->context->cart;
+        $customer = new Customer($cart->id_customer);
+
+        return $customer->secure_key;
+    }
+
+    protected function getCurrencyId()
+    {
+        $currency = $this->context->currency;
+        return (int) $currency->id;
+    }
+
+    /**
+     * The array of extra variables that will be used to attach to the order email.
+     *
+     * @return array
+     */
+    protected function getExtraVariables()
+    {
+        return array();
+    }
+
+    protected function getModuleDisplayName()
+    {
+        return $this->module->displayName;
+    }
+
+    /**
+     * The optional message that will be used to attach to the order.
+     *
+     * @return string
+     */
+    protected function getOptionalMessage()
+    {
+        return null;
+    }
+
+    /**
+     * The successful order status.
+     *
+     * @return mixed
+     */
+    protected function getOrderStateAcceptedPayment()
+    {
+        return Configuration::get('PS_OS_PAYMENT');
+    }
+
+    /**
+     * The flag that used to indicate that the PrestaShop need to
+     * round the card order total amount.
+     *
+     * If the flag is false, the PrestaShop will perform rounding.
+     * If the flag is true, the PrestaShop WILL NOT preform rounding.
+     *
+     * @return bool
+     */
+    protected function isNotNeededRoundingCardOrderTotal()
+    {
+        return false;
+    }
+
+    public function save()
+    {
+        $this->module->validateOrder(
+            $this->getCartId(),
+            $this->getOrderStateAcceptedPayment(),
+            $this->getCartOrderTotal(),
+            $this->getModuleDisplayName(),
+            $this->getOptionalMessage(),
+            $this->getExtraVariables(),
+            $this->getCurrencyId(),
+            $this->isNotNeededRoundingCardOrderTotal(),
+            $this->getCustomerSecureKey()
+        );
+    }
 }
