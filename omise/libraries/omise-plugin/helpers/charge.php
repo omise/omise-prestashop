9755e19d54994b406f0b23f5da5bb6e1b8117b35
Saroj Saengphongumphai <ss.saroj@gmail.com>
2017-01-22 15:38:23 +0700

    Add a helper function to check, is charge failed
    Add a helper function, isFailed(), to check whether the charge failed.

--- omise/libraries/omise-plugin/helpers/charge.php
+++ omise/libraries/omise-plugin/helpers/charge.php
@@ -32,5 +32,28 @@ if (! class_exists('OmisePluginHelperCharge')) {
 
             return true;
         }
+
+        /**
+         *
+         * @param \omise-php\OmiseCharge $charge
+         * @return boolean
+         */
+        public static function isFailed($charge)
+        {
+            if (! self::isChargeObject($charge)) {
+                return true;
+            }
+
+            if ((! is_null($charge['failure_code']) && $charge['failure_code'] !== '')
+                || (! is_null($charge['failure_message']) && $charge['failure_message'] !== '')) {
+                return true;
+            }
+
+            if (strtoupper($charge['status']) === 'FAILED') {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
