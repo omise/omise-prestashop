93308b27f2b196bd57c98650485ade2867ebb8c0
Saroj Saengphongumphai <ss.saroj@gmail.com>
2017-01-26 17:49:44 +0700

    Modify payment controller class, extend base class
    Modify a class, OmisePaymentModuleFrontController by extend from the
    base class, OmiseBasePaymentModuleFrontController.

--- omise/controllers/front/payment.php
+++ omise/controllers/front/payment.php
@@ -4,44 +4,16 @@ if (! defined('_PS_VERSION_')) {
 }
 
 if (defined('_PS_MODULE_DIR_')) {
-    require_once _PS_MODULE_DIR_ . 'omise/classes/charge.php';
-    require_once _PS_MODULE_DIR_ . 'omise/classes/payment_order.php';
+    require_once _PS_MODULE_DIR_ . 'omise/controllers/front/base.php';
 }
 
-class OmisePaymentModuleFrontController extends ModuleFrontController
+class OmisePaymentModuleFrontController extends OmiseBasePaymentModuleFrontController
 {
-    protected $charge;
-    public $display_column_left = false;
-    protected $error_message;
-
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->omise_charge = new Charge();
-        $this->payment_order = new PaymentOrder();
-        $this->setting = new Setting();
-    }
-
-    public function initContent()
-    {
-        parent::initContent();
-
-        $this->context->smarty->assign('error_message', $this->error_message);
-        $this->setTemplate('payment-error.tpl');
-    }
-
     public function postProcess()
     {
-        try {
-            $this->charge = $this->omise_charge->create();
-        } catch (Exception $e) {
-            $this->error_message = $e->getMessage();
-            return;
-        }
+        parent::postProcess();
 
-        if ($this->charge->isFailed()) {
-            $this->error_message = $this->charge->getErrorMessage();
+        if (! empty($this->error_message)) {
             return;
         }
 
@@ -53,10 +25,5 @@ class OmisePaymentModuleFrontController extends ModuleFrontController
             '&id_order=' . $this->module->currentOrder .
             '&key=' . $this->context->customer->secure_key
         );
-    }
-
-    protected function redirect()
-    {
-        Tools::redirect($this->redirect_after);
     }
 }
