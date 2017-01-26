de9fbf397349ee13d4ecf319971ad602403d2dd7
Saroj Saengphongumphai <ss.saroj@gmail.com>
2017-01-26 18:35:11 +0700

    Add a controller for processing 3-D Secure payment

---
+++ omise/controllers/front/threedomainsecurepayment.php
@@ -0,0 +1,24 @@
+<?php
+if (! defined('_PS_VERSION_')) {
+    exit();
+}
+
+if (defined('_PS_MODULE_DIR_')) {
+    require_once _PS_MODULE_DIR_ . 'omise/controllers/front/base.php';
+}
+
+class OmiseThreeDomainSecurePaymentModuleFrontController extends OmiseBasePaymentModuleFrontController
+{
+    public function postProcess()
+    {
+        $this->payment_order->saveAsProcessing();
+
+        parent::postProcess();
+
+        if (! empty($this->error_message)) {
+            return;
+        }
+
+        $this->setRedirectAfter($this->charge->getAuthorizeUrl());
+    }
+}
