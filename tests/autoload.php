2db48203a39f7f38d57398e90b950d62c48c7dfe
Saroj Saengphongumphai <ss.saroj@gmail.com>
2017-02-21 19:19:43 +0700

    Use composer to load base class automatically
    Before change, to load base class in part of unit testing, it need to
    manually defined every base classes in file, tests/autoload.php, for
    reference.
    
    This change use a feature of composer to automatically load base
    class.
    
    Note:
    A line, require_once __DIR__ . '/../vendor/autoload.php';, in file,
    tests/autoload.php, is required to link with autoload of composer.

--- tests/autoload.php
+++ tests/autoload.php
@@ -4,24 +4,3 @@ require_once __DIR__ . '/../vendor/autoload.php';
 if (! defined('_PS_VERSION_')) {
     define('_PS_VERSION_', 'TEST_VERSION');
 }
-
-function autoload($class)
-{
-    static $classes = null;
-
-    if ($classes === null) {
-        $classes = array(
-            'Charge' => 'classes/charge.php',
-            'CheckoutForm' => 'checkout_form.php',
-            'Omise' => 'omise.php',
-            'OmisePaymentModuleFrontController' => 'controllers/front/payment.php',
-            'Setting' => 'setting.php',
-        );
-    }
-
-    if (isset($classes[$class])) {
-        require __DIR__ . '/../omise/' . $classes[$class];
-    }
-}
-
-spl_autoload_register('autoload');
