954f412dd24cf8f4c21f3cfc4b2b87dcb5a790f8
Saroj Saengphongumphai <ss.saroj@gmail.com>
2017-03-02 18:07:58 +0700

    Add a reference of Order ID and charege ID to DB
    When create 3-D Secure Omise charge, add a reference between PrestaShop
    order ID and Omise charege ID to the database table.
    
    The reference will be used to check the status of Omise change at the
    step of redirect back from Omise API.

--- omise/controllers/front/threedomainsecurepayment.php
+++ omise/controllers/front/threedomainsecurepayment.php
@@ -4,11 +4,42 @@ if (! defined('_PS_VERSION_')) {
 }
 
 if (defined('_PS_MODULE_DIR_')) {
+    require_once _PS_MODULE_DIR_ . 'omise/classes/omise_transaction_model.php';
     require_once _PS_MODULE_DIR_ . 'omise/controllers/front/base.php';
 }
 
 class OmiseThreeDomainSecurePaymentModuleFrontController extends OmiseBasePaymentModuleFrontController
 {
+    protected $omise_transaction_model;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->setOmiseTransactionModel(new OmiseTransactionModel());
+    }
+
+    /**
+     * Add a reference between PrestaShop order ID and Omise charge ID
+     * to a database table.
+     *
+     * This reference will be used to check the status of Omise change
+     * at the step of redirect back from Omise API.
+     * (@see OmiseReturnModuleFrontController::isChargeValid())
+     *
+     * @param string $id_charge The Omise charge ID.
+     * @param int $id_order The PrestaShop order ID.
+     *
+     * @return bool
+     */
+    protected function addOmiseTransaction($id_charge, $id_order)
+    {
+        $this->omise_transaction_model->id_charge = $id_charge;
+        $this->omise_transaction_model->id_order = $id_order;
+
+        return $this->omise_transaction_model->add();
+    }
+
     public function postProcess()
     {
         $this->validateCart();
@@ -21,6 +52,13 @@ class OmiseThreeDomainSecurePaymentModuleFrontController extends OmiseBasePaymen
             return;
         }
 
+        $this->addOmiseTransaction($this->charge->getId(), Order::getOrderByCartId($this->context->cart->id));
+
         $this->setRedirectAfter($this->charge->getAuthorizeUri());
+    }
+
+    public function setOmiseTransactionModel($omise_transaction_model)
+    {
+        $this->omise_transaction_model = $omise_transaction_model;
     }
 }
