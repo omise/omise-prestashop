84a0a1dd2b8624c944f4eec7d345214c64a426c2
Saroj Saengphongumphai <ss.saroj@gmail.com>
2017-03-02 18:13:29 +0700

    Validate order & charge, when redirect back
    Modify a class, OmiseReturnModuleFrontController. Develop functions to
    validate PrestaShop order information and Omise charge information
    before update the order status to be success.
    
    If an error occurred or the information is not valid, the error message
    will be displayed at the front-end.

--- omise/controllers/front/return.php
+++ omise/controllers/front/return.php
@@ -4,12 +4,66 @@ if (! defined('_PS_VERSION_')) {
 }
 
 if (defined('_PS_MODULE_DIR_')) {
-    require_once _PS_MODULE_DIR_ . 'omise/classes/payment_order.php';
+    require_once _PS_MODULE_DIR_ . 'omise/classes/omise_transaction_model.php';
+    require_once _PS_MODULE_DIR_ . 'omise/controllers/front/base.php';
+    require_once _PS_MODULE_DIR_ . 'omise/omise.php';
 }
 
-class OmiseReturnModuleFrontController extends ModuleFrontController
+class OmiseReturnModuleFrontController extends OmiseBasePaymentModuleFrontController
 {
-    public $display_column_left = false;
+    protected $order;
+
+    protected function isChargeValid($id_order)
+    {
+        $omise_transaction = new OmiseTransactionModel();
+
+        $id_charge = $omise_transaction->getIdCharge($id_order);
+
+        try {
+            $this->charge = $this->omise_charge->retrieve($id_charge);
+        } catch (Exception $e) {
+            $this->error_message = $e->getMessage();
+            return false;
+        }
+
+        if ($this->charge->isFailed()) {
+            $this->error_message = $this->omise_charge->getErrorMessage();
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function isPaymentValid($id_order)
+    {
+        $this->order = new Order($id_order);
+
+        if (Validate::isLoadedObject($this->order) == false) {
+            $this->error_message = $this->l('Order not found.');
+            return false;
+        }
+
+        $this->order_reference = $this->order->reference;
+
+        if ($this->order->payment != Omise::MODULE_DISPLAY_NAME) {
+            $this->error_message = $this->l('Payment method is invalid.');
+            return false;
+        }
+
+        if ($this->order->current_state != $this->payment_order->getOrderStateProcessingInProgress()
+            && $this->order->current_state != $this->payment_order->getOrderStateAcceptedPayment()) {
+
+            $this->error_message = $this->l('Order status is invalid.');
+
+            return false;
+        }
+
+        if ($this->isChargeValid($id_order) == false) {
+            return false;
+        }
+
+        return true;
+    }
 
     public function postProcess()
     {
@@ -18,8 +72,11 @@ class OmiseReturnModuleFrontController extends ModuleFrontController
         $id_order = Tools::getValue('id_order');
         $key = Tools::getValue('key');
 
-        $payment_order = new PaymentOrder();
-        $payment_order->updateStateToBeSuccess($id_order);
+        if ($this->isPaymentValid($id_order) == false) {
+            return;
+        }
+
+        $this->payment_order->updateStateToBeSuccess($this->order);
 
         $this->setRedirectAfter('index.php?controller=order-confirmation' .
             '&id_cart=' . $id_cart .
