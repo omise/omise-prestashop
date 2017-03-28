da81744f6f2397a1d30bdd39b2306d41383c6a8c
Saroj Saengphongumphai <ss.saroj@gmail.com>
2017-03-28 20:10:42 +0700

    Add a new controller class for internet banking

---
+++ omise/controllers/front/internetbankingpayment.php
@@ -0,0 +1,39 @@
+<?php
+if (! defined('_PS_VERSION_')) {
+    exit();
+}
+
+if (defined('_PS_MODULE_DIR_')) {
+    require_once _PS_MODULE_DIR_ . 'omise/classes/omise_transaction_model.php';
+    require_once _PS_MODULE_DIR_ . 'omise/controllers/front/base.php';
+}
+
+class OmiseInternetBankingPaymentModuleFrontController extends OmiseBasePaymentModuleFrontController
+{
+    public function postProcess()
+    {
+        $this->validateCart();
+
+        $this->payment_order->saveAsProcessing();
+
+        try {
+            $this->charge = $this->omise_charge->createInternetBanking(Tools::getValue('offsite'));
+        } catch (Exception $e) {
+            $this->error_message = $e->getMessage();
+            return;
+        }
+
+        if ($this->charge->isFailed()) {
+            $this->error_message = $this->charge->getErrorMessage();
+            return;
+        }
+
+        if (! empty($this->error_message)) {
+            return;
+        }
+
+        $this->addOmiseTransaction($this->charge->getId(), Order::getOrderByCartId($this->context->cart->id));
+
+        $this->setRedirectAfter($this->charge->getAuthorizeUri());
+    }
+}
