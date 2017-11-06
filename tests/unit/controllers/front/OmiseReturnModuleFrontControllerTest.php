f443c21b905689e3430bab03f1770ebe0aa58e19
Saroj Sangphongamphai <ss.saroj@gmail.com>
2017-11-06 18:56:11 +0700

    Partially add unit test for a controller class
    Partially add unit test cases for a controller class,
    OmiseReturnModuleFrontController.
    
    The unit test for this class can not be additionally added because this
    class has a hard dependency (new keyword) with a class, Order, and access
    to many public properties of Order class (e.g. $this->order->reference).
    
    This condition makes the mocking public property for hard dependency
    difficult.
    
    Another solution is adding a setter for the $this->order but this
    solution needs to modify the actual code that it is not requires in this
    time.

---
+++ tests/unit/controllers/front/OmiseReturnModuleFrontControllerTest.php
@@ -0,0 +1,79 @@
+<?php
+use \Mockery as m;
+
+class OmiseReturnModuleFrontControllerTest extends PHPUnit_Framework_TestCase
+{
+    private $omise_return_module_front_controller;
+
+    public function setup()
+    {
+        $this->getMockBuilder(get_class(new stdClass()))
+            ->setMockClassName('OmiseBasePaymentModuleFrontController')
+            ->setMethods(
+                array(
+                    '__construct',
+                    'addOmiseTransaction',
+                    'l',
+                    'postProcess',
+                    'setRedirectAfter',
+                    'validateCart',
+                )
+            )
+            ->getMock();
+
+        m::mock('overload:\Order');
+
+        m::mock('alias:\Tools')->shouldIgnoreMissing();
+
+        m::mock('alias:\Validate')->shouldIgnoreMissing();
+
+        $this->omise_return_module_front_controller = new OmiseReturnModuleFrontController();
+        $this->omise_return_module_front_controller->payment_order = $this->getMockedPaymentOrder();
+    }
+
+    public function testPostProcess_orderIsNotFound_errorMessageMustBeDefined()
+    {
+        m::mock('alias:\Validate')
+            ->shouldReceive('isLoadedObject')
+            ->andReturn(false);
+
+        $this->omise_return_module_front_controller
+            ->expects($this->once())
+            ->method('l')
+            ->with('Order not found.');
+
+        $this->omise_return_module_front_controller->postProcess();
+    }
+
+    public function testPostProcess_returnFromOmiseApi_getValueThatPassedBackFromOmiseApi()
+    {
+        m::mock('alias:\Tools')
+            ->shouldReceive('getValue')->with('id_cart')->andReturn('idCart')
+            ->shouldReceive('getValue')->with('id_module')->andReturn('idModule')
+            ->shouldReceive('getValue')->with('id_order')->andReturn('idOrder')
+            ->shouldReceive('getValue')->with('key')->andReturn('key');
+
+        $this->omise_return_module_front_controller->postProcess();
+    }
+
+    private function getMockedPaymentOrder()
+    {
+        $payment_order = $this->getMockBuilder(get_class(new stdClass()))
+            ->setMethods(
+                array(
+                    'saveAsProcessing',
+                    'updatePaymentTransactionId',
+                    'updateStateToBeCanceled',
+                    'updateStateToBeSuccess',
+                )
+            )
+            ->getMock();
+
+        return $payment_order;
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+}
