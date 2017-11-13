dc4a32e796f1d9a6debf4317442af3d4fc8d6000
Saroj Sangphongamphai <ss.saroj@gmail.com>
2017-11-13 13:46:14 +0700

    Change flow when creating payment by card
    This commit affects normal card payment only. 3DS payment is not
    affected.
    
    The objective of this commit is to append PrestaShop order ID to the
    metadata of Omise charge.
    
    The flow has been changed to save an order as processing at the first
    step and uses that generated order ID to append to the metadata of Omise charge.

--- tests/unit/controllers/front/OmisePaymentModuleFrontControllerTest.php
+++ tests/unit/controllers/front/OmisePaymentModuleFrontControllerTest.php
@@ -1,6 +1,9 @@
 <?php
-class OmisePaymentModuleFrontControllerTest extends PHPUnit_Framework_TestCase
+use \Mockery as m;
+
+class OmisePaymentModuleFrontControllerTest extends Mockery\Adapter\Phpunit\MockeryTestCase
 {
+    private $omise_base_payment_module_front_controller;
     private $omise_payment_module_front_controller;
     private $payment_order;
 
@@ -8,7 +11,11 @@ class OmisePaymentModuleFrontControllerTest extends PHPUnit_Framework_TestCase
     {
         $unit_test_helper = new UnitTestHelper();
 
-        $unit_test_helper->getMockedOmiseBasePaymentModuleFrontController();
+        $this->omise_base_payment_module_front_controller = $unit_test_helper->getMockedOmiseBasePaymentModuleFrontController();
+
+        m::mock('alias:\Order')
+            ->shouldReceive('getIdByCartId')
+            ->andReturn('id_order');
 
         $this->omise_payment_module_front_controller = new OmisePaymentModuleFrontController();
         $this->omise_payment_module_front_controller->charge = $unit_test_helper->getMockedCharge();
@@ -18,27 +25,50 @@ class OmisePaymentModuleFrontControllerTest extends PHPUnit_Framework_TestCase
         $this->omise_payment_module_front_controller->setting = $unit_test_helper->getMockedSetting();
     }
 
-    public function testPostProcess_errorOccurred_noAnyOrderHasBeenSaved()
-    {
-        $this->omise_payment_module_front_controller->error_message = 'errorMessage';
-
-        $this->omise_payment_module_front_controller->payment_order
-            ->expects($this->never())
-            ->method('save');
-
-        $this->omise_payment_module_front_controller->postProcess();
-    }
-
-    public function testPostProcess_noErrorOccurred_saveTheOrder()
+    public function testPostProcess_createCharge_saveAnOrderWithTheOrderStatusIsProcessing()
     {
         $this->omise_payment_module_front_controller->payment_order
             ->expects($this->once())
             ->method('save')
             ->with(
-                'orderStateAcceptedPayment',
-                'title',
+                'orderStatusProcessingInProgress',
+                'title'
+            );
+
+        $this->omise_payment_module_front_controller->postProcess();
+    }
+
+    public function testPostProcess_chargeResultIsNotEmpty_saveOmiseChargeIdToPrestaShopOrder()
+    {
+        $this->omise_payment_module_front_controller->payment_order
+            ->expects($this->once())
+            ->method('updatePaymentTransactionId')
+            ->with(
+                'id_order',
                 'omiseChargeId'
             );
+
+        $this->omise_payment_module_front_controller->postProcess();
+    }
+
+    public function testPostProcess_createChargeIsError_updateOrderStatusToBeCanceled()
+    {
+        $this->omise_payment_module_front_controller->error_message = 'errorMessage';
+
+        $this->omise_payment_module_front_controller->payment_order
+            ->expects($this->once())
+            ->method('updateStateToBeCanceled');
+
+        $this->omise_payment_module_front_controller->postProcess();
+    }
+
+    public function testPostProcess_createChargeIsSuccess_redirectToOrderConfirmationPage()
+    {
+        $this->omise_payment_module_front_controller->error_message = '';
+
+        $this->omise_payment_module_front_controller->payment_order
+            ->expects($this->once())
+            ->method('updateStateToBeSuccess');
 
         $this->omise_payment_module_front_controller->postProcess();
     }
