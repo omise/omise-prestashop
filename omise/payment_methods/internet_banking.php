c87200ae758f44064c3e584b5eb2614b40cd5398
Jon Randy <jonrandy@gmail.com>
2020-01-23 12:25:33 +0700

    Removes now unnecessary internet banking front controller

--- omise/payment_methods/internet_banking.php
+++ omise/payment_methods/internet_banking.php
@@ -18,4 +18,41 @@ class OmisePaymentMethod_InternetBanking extends OmisePaymentMethod
         $restrictedToCurrencies = array('thb')
     ;
 
+    public static function processPayment($controller, $context)
+    {
+        
+        $c = $controller;
+
+        $c->validateCart();
+
+        $c->payment_order->save(
+            $c->payment_order->getOrderStateProcessingInProgress(),
+            self::DEFAULT_TITLE
+        );
+
+        try {
+            $c->charge = $c->omise_charge->createOffsite(Tools::getValue('offsite'));
+        } catch (Exception $e) {
+            $c->error_message = $e->getMessage();
+            return;
+        }
+
+        $id_order = Order::{PRESTASHOP_GET_ORDER_ID_METHOD}($context->cart->id);
+
+        $c->payment_order->updatePaymentTransactionId($id_order, $c->charge->getId());
+
+        if ($c->charge->isFailed()) {
+            $c->error_message = $c->charge->getErrorMessage();
+            return;
+        }
+
+        if (!empty($c->error_message)) {
+            return;
+        }
+
+        $c->addOmiseTransaction($c->charge->getId(), $id_order);
+
+        $c->setRedirectAfter($c->charge->getAuthorizeUri());
+    }
+
 }
