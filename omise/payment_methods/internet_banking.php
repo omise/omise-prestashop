073b761a234861a3812feb5f9838b0d0c614758f
Jon Randy <jonrandy@gmail.com>
2020-01-23 16:24:11 +0700

    Makes offsite payments share common code

--- omise/payment_methods/internet_banking.php
+++ omise/payment_methods/internet_banking.php
@@ -1,6 +1,10 @@
 <?php
 
-class OmisePaymentMethod_InternetBanking extends OmisePaymentMethod
+if (defined('_PS_MODULE_DIR_')) {
+    require_once _PS_MODULE_DIR_ . 'omise/payment_methods/_offsite.php';
+}
+
+class OmisePaymentMethod_InternetBanking extends OmiseOffsitePaymentMethod
 {
 
     const
@@ -20,39 +24,7 @@ class OmisePaymentMethod_InternetBanking extends OmisePaymentMethod
 
     public static function processPayment($controller, $context)
     {
-        
-        $c = $controller;
-
-        $c->validateCart();
-
-        $c->payment_order->save(
-            $c->payment_order->getOrderStateProcessingInProgress(),
-            self::getTitle()
-        );
-
-        try {
-            $c->charge = $c->omise_charge->createOffsite(Tools::getValue('offsite'));
-        } catch (Exception $e) {
-            $c->error_message = $e->getMessage();
-            return;
-        }
-
-        $id_order = Order::{PRESTASHOP_GET_ORDER_ID_METHOD}($context->cart->id);
-
-        $c->payment_order->updatePaymentTransactionId($id_order, $c->charge->getId());
-
-        if ($c->charge->isFailed()) {
-            $c->error_message = $c->charge->getErrorMessage();
-            return;
-        }
-
-        if (!empty($c->error_message)) {
-            return;
-        }
-
-        $c->addOmiseTransaction($c->charge->getId(), $id_order);
-
-        $c->setRedirectAfter($c->charge->getAuthorizeUri());
+        parent::processOffsitePayment(Tools::getValue('offsite'), $controller, $context);
     }
 
 }
